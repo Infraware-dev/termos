c4de2e3af78fdd6d2993512a58c5b9de143020f1
bitstreamshaman <nicolas.shane.lynch@gmail.com>
2025-11-18 10:37:52 +0000

    reafctor: formatted all files with  the "ruff format" command

--- backend/src/agents/aws/__init__.py
+++ backend/src/agents/aws/__init__.py
@@ -1,3 +1,3 @@
 from .tools import get_ip_aws
 
-__all__ = ['get_ip_aws']
+__all__ = ["get_ip_aws"]
--- backend/src/agents/gcp/__init__.py
+++ backend/src/agents/gcp/__init__.py
@@ -1,3 +1,3 @@
 from .tools import get_ip_gcp
 
-__all__ = ['get_ip_gcp']
+__all__ = ["get_ip_gcp"]
